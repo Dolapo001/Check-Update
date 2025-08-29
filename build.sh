3e9f79797da5c1d62d8fd18181ce4ebbcbc736f9
Dolapo001 <adedolapovictoria927@gmail.com>
2025-08-29 08:19:33 +0100

    first

--- build.sh
+++ build.sh
@@ -13,6 +13,9 @@ python manage.py collectstatic --noinput
 # Apply migrations
 python manage.py migrate
 
+mkdir -p static/img static/css
+echo "iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAABCElEQVRYhe2WMU7DQBBFn1KkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRYoUKVKkSJEiRf4N3wDqH1o4x7JxAAAAAElFTkSuQmCC" | base64 -d > static/img/logo.jpg
+
 # Create superuser if it doesn't exist
 python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
