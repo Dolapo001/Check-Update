5377dcc8bcfc326254b46a3becdcd90584447c2f
Dolapo001 <adedolapovictoria927@gmail.com>
2025-09-02 12:28:01 +0100

    first

--- build.sh
+++ build.sh
@@ -20,14 +20,14 @@ python manage.py backfill_content
 python manage.py migrate
 
 
-# Create superuser if it doesn't exist
-python manage.py shell <<EOF
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username="${DJANGO_SUPERUSER_USERNAME}").exists():
-    User.objects.create_superuser(
-        username="${DJANGO_SUPERUSER_USERNAME}",
-        email="${DJANGO_SUPERUSER_EMAIL}",
-        password="${DJANGO_SUPERUSER_PASSWORD}"
-    )
-EOF
+## Create superuser if it doesn't exist
+#python manage.py shell <<EOF
+#from django.contrib.auth import get_user_model
+#User = get_user_model()
+#if not User.objects.filter(username="${DJANGO_SUPERUSER_USERNAME}").exists():
+#    User.objects.create_superuser(
+#        username="${DJANGO_SUPERUSER_USERNAME}",
+#        email="${DJANGO_SUPERUSER_EMAIL}",
+#        password="${DJANGO_SUPERUSER_PASSWORD}"
+#    )
+#EOF
